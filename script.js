374ab2c7c81a2b0785c3b2d2d17cdac5ff7c1319
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-05-30 00:37:57 +0600

    dom element select

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-const btn = document.querySelector('.button');
+const wrapper = document.querySelector('.wrapper'),
+	musicImg = wrapper.querySelector('.image-area img'),
+	musicName = wrapper.querySelector('.song-details .name'),
+	musicArtist = wrapper.querySelector('.song-details .artist');
