1fd1ca5c01f96907b650132788bea669e9819261
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-05-30 00:37:33 +0600

    music list add

---
+++ music-list.js
@@ -0,0 +1,21 @@
+// Example music data
+const musicData = [
+	{
+		name: 'Sick Of (Official Lyrics)',
+		artist: 'Herman',
+		image: './audio/harman.jpg',
+		src: './audio/Herman - Sick Of (Official Lyrics) (128 kbps).mp3',
+	},
+	{
+		name: 'Song Name 2',
+		artist: 'Artist 2',
+		image: 'path/to/image2.jpg',
+		src: 'path/to/audio2.mp3',
+	},
+	{
+		name: 'Song Name 3',
+		artist: 'Artist 3',
+		image: 'path/to/image3.jpg',
+		src: 'path/to/audio3.mp3',
+	},
+];
