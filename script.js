51a8a6eff78d10dd43536d71151b2ad372c7f442
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-06-04 19:41:48 +0600

    progress bar & music functionality

--- script.js
+++ script.js
@@ -8,9 +8,13 @@ function musicPlayer() {
 		nextBtn = wrapper.querySelector('#next'),
 		prevBtn = wrapper.querySelector('#prev'),
 		musicListBtn = wrapper.querySelector('#more-music'),
+		musicListBox = wrapper.querySelector('.music-list'),
+		musicBoxClose = wrapper.querySelector('#close'),
 		musicDirection = wrapper.querySelector('#repeat-plist'),
+		startTime = wrapper.querySelector('.current-time'),
+		totalTime = wrapper.querySelector('.total-time'),
+		progressArea = wrapper.querySelector('.progress-area'),
 		progressLine = wrapper.querySelector('.progress-bar');
-
 	let musicIndex = 0;
 
 	window.addEventListener('load', () => {
@@ -48,19 +52,16 @@ function musicPlayer() {
 	});
 
 	function nextAndPrevSong(value) {
-		if (musicIndex >= 0 && musicIndex < musicData.length) {
-			if (value == 'next') {
-				musicIndex++;
-				loadMusic(musicIndex);
-				playMusic();
-			} else {
-				if (musicIndex == 0) {
-					return;
-				}
-				musicIndex--;
-				loadMusic(musicIndex);
-				playMusic();
-			}
+		if (value == 'next') {
+			musicIndex++;
+			musicIndex = musicIndex < musicData.length ? musicIndex : 0;
+			loadMusic(musicIndex);
+			playMusic();
+		} else {
+			musicIndex--;
+			musicIndex = musicIndex == -1 ? musicData.length - 1 : musicIndex;
+			loadMusic(musicIndex);
+			playMusic();
 		}
 	}
 
@@ -70,6 +71,48 @@ function musicPlayer() {
 
 	prevBtn.addEventListener('click', () => {
 		nextAndPrevSong();
+	});
+
+	musicListBtn.addEventListener('click', () => {
+		musicListBox.classList.add('active');
+	});
+
+	musicBoxClose.addEventListener('click', () => {
+		musicListBox.classList.remove('active');
+	});
+
+	audio.addEventListener('timeupdate', (even) => {
+		const currTime = even.target.currentTime;
+		const duration = even.target.duration;
+		const percentage = (currTime / duration) * 100;
+
+		progressLine.style.width = `${percentage}%`;
+		console.log(even);
+
+		audio.addEventListener('loadeddata', () => {
+			let audioDuration = audio.duration;
+			let totalMin = Math.floor(audioDuration / 60);
+			let totalSec = Math.floor(audioDuration % 60);
+			totalTime.innerText = `${totalMin}:${totalSec}`;
+			console.log(audioDuration);
+		});
+
+		let currentMin = Math.floor(currTime / 60);
+		let currentSec = Math.floor(currTime % 60);
+		if (currentSec < 10) {
+			currentSec = `0${currentSec}`;
+		}
+		startTime.innerText = `${currentMin}:${currentSec}`;
+	});
+
+	progressArea.addEventListener('click', (e) => {
+		let progressWidthVal = progressArea.clientWidth;
+		let clickedOffSetX = e.offsetX;
+		let songDuration = audio.duration;
+		console.log((clickedOffSetX / progressWidthVal) * songDuration);
+		audio.target.currentTime =
+			(clickedOffSetX / progressWidthVal) * songDuration;
+		// audio.currentTime = 4.187971;
 	});
 }
 
