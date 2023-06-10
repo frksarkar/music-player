f732542c624c24a3305480bb722ef6a800ab05ae
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-06-10 23:42:27 +0600

    some change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function musicPlayer() {
 		musicImg = wrapper.querySelector('.image-area img'),
 		musicName = wrapper.querySelector('.song-details .name'),
 		musicArtist = wrapper.querySelector('.song-details .artist'),
-		audio = wrapper.querySelector('#main-audio'),
+		myAudio = wrapper.querySelector('#main-audio'),
 		playPauseBtn = wrapper.querySelector('.play-pause'),
 		nextBtn = wrapper.querySelector('#next'),
 		prevBtn = wrapper.querySelector('#prev'),
@@ -15,7 +15,7 @@ function musicPlayer() {
 		totalTime = wrapper.querySelector('.total-time'),
 		progressArea = wrapper.querySelector('.progress-area'),
 		progressLine = wrapper.querySelector('.progress-bar');
-	let musicIndex = 0;
+	let musicIndex = 1;
 
 	window.addEventListener('load', () => {
 		loadMusic(musicIndex);
@@ -27,7 +27,7 @@ function musicPlayer() {
 		musicName.innerText = musicData[index].name;
 		musicArtist.innerText = musicData[index].artist;
 		musicImg.src = musicData[index].image;
-		audio.src = musicData[index].src;
+		myAudio.src = musicData[index].src;
 	}
 
 	// play pause button
@@ -35,19 +35,17 @@ function musicPlayer() {
 	function pauseMusic() {
 		playPauseBtn.querySelector('i').innerText = 'pause';
 		wrapper.classList.remove('paused');
-		audio.pause();
+		myAudio.pause();
 	}
 
 	function playMusic() {
 		playPauseBtn.querySelector('i').innerText = 'play_arrow';
-		console.log('play');
 		wrapper.classList.add('paused');
-		audio.play();
+		myAudio.play();
 	}
 
 	playPauseBtn.addEventListener('click', () => {
 		const isMusicPaused = wrapper.classList.contains('paused');
-		console.log(isMusicPaused);
 		isMusicPaused ? pauseMusic() : playMusic();
 	});
 
@@ -81,21 +79,17 @@ function musicPlayer() {
 		musicListBox.classList.remove('active');
 	});
 
-	audio.addEventListener('timeupdate', (even) => {
-		const currTime = even.target.currentTime;
-		const duration = even.target.duration;
+	myAudio.addEventListener('timeupdate', (event) => {
+		const currTime = event.target.currentTime;
+		const duration = event.target.duration;
 		const percentage = (currTime / duration) * 100;
 
 		progressLine.style.width = `${percentage}%`;
-		console.log(even);
 
-		audio.addEventListener('loadeddata', () => {
-			let audioDuration = audio.duration;
-			let totalMin = Math.floor(audioDuration / 60);
-			let totalSec = Math.floor(audioDuration % 60);
-			totalTime.innerText = `${totalMin}:${totalSec}`;
-			console.log(audioDuration);
-		});
+		let audioDuration = myAudio.duration;
+		let totalMin = Math.floor(audioDuration / 60);
+		let totalSec = Math.floor(audioDuration % 60);
+		totalTime.innerText = `${totalMin}:${totalSec}`;
 
 		let currentMin = Math.floor(currTime / 60);
 		let currentSec = Math.floor(currTime % 60);
@@ -105,14 +99,40 @@ function musicPlayer() {
 		startTime.innerText = `${currentMin}:${currentSec}`;
 	});
 
+	myAudio.addEventListener('canplaythrough', () => {
+		let audioDuration = myAudio.duration;
+		let totalMin = Math.floor(audioDuration / 60);
+		let totalSec = Math.floor(audioDuration % 60);
+		totalTime.innerText = `${totalMin}:${totalSec}`;
+	});
+	// myAudio.addEventListener('timeupdate', (even) => {
+	// 	const currTime = even.target.currentTime;
+	// 	const duration = even.target.duration;
+	// 	const percentage = (currTime / duration) * 100;
+
+	// 	progressLine.style.width = `${percentage}%`;
+
+	// 	myAudio.addEventListener('loadeddata', () => {
+	// 		let audioDuration = myAudio.duration;
+	// 		let totalMin = Math.floor(audioDuration / 60);
+	// 		let totalSec = Math.floor(audioDuration % 60);
+	// 		totalTime.innerText = `${totalMin}:${totalSec}`;
+	// 	});
+
+	// 	let currentMin = Math.floor(currTime / 60);
+	// 	let currentSec = Math.floor(currTime % 60);
+	// 	if (currentSec < 10) {
+	// 		currentSec = `0${currentSec}`;
+	// 	}
+	// 	startTime.innerText = `${currentMin}:${currentSec}`;
+	// });
+
 	progressArea.addEventListener('click', (e) => {
 		let progressWidthVal = progressArea.clientWidth;
 		let clickedOffSetX = e.offsetX;
-		let songDuration = audio.duration;
-		console.log((clickedOffSetX / progressWidthVal) * songDuration);
-		audio.target.currentTime =
+		let songDuration = myAudio.duration;
+		myAudio.currentTime =
 			(clickedOffSetX / progressWidthVal) * songDuration;
-		// audio.currentTime = 4.187971;
 	});
 }
 
