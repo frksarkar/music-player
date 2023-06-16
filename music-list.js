f47b06982f1bb6264dc714fb07795471e61aa0b4
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-06-17 00:01:10 +0600

    some change

--- music-list.js
+++ music-list.js
@@ -1,21 +1,24 @@
 // Example music data
 const musicData = [
-	{
-		name: 'Sick Of (Official Lyrics)',
-		artist: 'Herman',
-		image: './audio/harman.jpg',
-		src: './audio/Herman - Sick Of (Official Lyrics) (128 kbps).mp3',
-	},
-	{
-		name: 'Hum Tumko Nigahon Mein',
-		artist: 'Udit Narayan',
-		image: './audio/Hum Tumko Nigahon Mein.jpg',
-		src: './audio/Hum Tumko Nigahon Mein.mp3',
-	},
-	{
-		name: 'Dil Ne Ye Kaha Hai Dil Se',
-		artist: 'Sonu Nigam',
-		image: './audio/Dil Ne Ye Kaha Hai Dil Se.jpg',
-		src: './audio/Dil Ne Ye Kaha Hai Dil Se.mp3',
-	},
+    {
+        id: 'music-1',
+        name: 'Sick Of (Official Lyrics)',
+        artist: 'Herman',
+        image: './audio/harman.jpg',
+        src: './audio/Herman - Sick Of (Official Lyrics) (128 kbps).mp3',
+    },
+    {
+        id: 'music-2',
+        name: 'Hum Tumko Nigahon Mein',
+        artist: 'Udit Narayan',
+        image: './audio/Hum Tumko Nigahon Mein.jpg',
+        src: './audio/Hum Tumko Nigahon Mein.mp3',
+    },
+    {
+        id: 'music-3',
+        name: 'Dil Ne Ye Kaha Hai Dil Se',
+        artist: 'Sonu Nigam',
+        image: './audio/Dil Ne Ye Kaha Hai Dil Se.jpg',
+        src: './audio/Dil Ne Ye Kaha Hai Dil Se.mp3',
+    },
 ];
--- script.js
+++ script.js
@@ -1,184 +1,206 @@
 function musicPlayer() {
-	const wrapper = document.querySelector('.wrapper'),
-		musicImg = wrapper.querySelector('.image-area img'),
-		musicName = wrapper.querySelector('.song-details .name'),
-		musicArtist = wrapper.querySelector('.song-details .artist'),
-		myAudio = wrapper.querySelector('#main-audio'),
-		playPauseBtn = wrapper.querySelector('.play-pause'),
-		nextBtn = wrapper.querySelector('#next'),
-		prevBtn = wrapper.querySelector('#prev'),
-		musicListBtn = wrapper.querySelector('#more-music'),
-		musicListBox = wrapper.querySelector('.music-list'),
-		musicBoxClose = wrapper.querySelector('#close'),
-		musicDirectionBtn = wrapper.querySelector('#repeat-plist'),
-		startTime = wrapper.querySelector('.current-time'),
-		totalTime = wrapper.querySelector('.total-time'),
-		progressArea = wrapper.querySelector('.progress-area'),
-		progressLine = wrapper.querySelector('.progress-bar');
-	let musicIndex = 0;
+    const wrapper = document.querySelector('.wrapper');
+    const musicImg = wrapper.querySelector('.image-area img');
+    const musicName = wrapper.querySelector('.song-details .name');
+    const musicArtist = wrapper.querySelector('.song-details .artist');
+    const myAudio = wrapper.querySelector('#main-audio');
+    const playPauseBtn = wrapper.querySelector('.play-pause');
+    const nextBtn = wrapper.querySelector('#next');
+    const prevBtn = wrapper.querySelector('#prev');
+    const musicListBtn = wrapper.querySelector('#more-music');
+    const musicListBox = wrapper.querySelector('.music-list');
+    const musicBoxClose = wrapper.querySelector('#close');
+    const musicDirectionBtn = wrapper.querySelector('#repeat-plist');
+    const startTime = wrapper.querySelector('.current-time');
+    const totalTime = wrapper.querySelector('.total-time');
+    const progressArea = wrapper.querySelector('.progress-area');
+    const progressLine = wrapper.querySelector('.progress-bar');
+    let musicIndex = 0;
 
-	window.addEventListener('load', () => {
-		loadMusic(musicIndex);
-	});
+    window.addEventListener('load', () => {
+        loadMusic(musicIndex);
+    });
 
-	// load music function
+    // load music function
 
-	function loadMusic(index) {
-		musicName.innerText = musicData[index].name;
-		musicArtist.innerText = musicData[index].artist;
-		musicImg.src = musicData[index].image;
-		myAudio.src = musicData[index].src;
-	}
+    function loadMusic(index) {
+        musicName.innerText = musicData[index].name;
+        musicArtist.innerText = musicData[index].artist;
+        musicImg.src = musicData[index].image;
+        myAudio.src = musicData[index].src;
+    }
 
-	// play pause button
+    // play pause button
 
-	function pauseMusic() {
-		playPauseBtn.querySelector('i').innerText = 'pause';
-		wrapper.classList.remove('paused');
-		myAudio.pause();
-	}
+    function pauseMusic() {
+        playPauseBtn.querySelector('i').innerText = 'pause';
+        wrapper.classList.remove('paused');
+        myAudio.pause();
+    }
 
-	function playMusic() {
-		playPauseBtn.querySelector('i').innerText = 'play_arrow';
-		wrapper.classList.add('paused');
-		myAudio.play();
-	}
+    function playMusic() {
+        playPauseBtn.querySelector('i').innerText = 'play_arrow';
+        wrapper.classList.add('paused');
+        myAudio.play();
+    }
 
-	playPauseBtn.addEventListener('click', () => {
-		const isMusicPaused = wrapper.classList.contains('paused');
-		isMusicPaused ? pauseMusic() : playMusic();
-	});
+    playPauseBtn.addEventListener('click', () => {
+        const isMusicPaused = wrapper.classList.contains('paused');
+        isMusicPaused ? pauseMusic() : playMusic();
+    });
 
-	function nextAndPrevSong(value) {
-		if (value == 'next') {
-			musicIndex++;
-			musicIndex = musicIndex < musicData.length ? musicIndex : 0;
-			loadMusic(musicIndex);
-			playMusic();
-		} else {
-			musicIndex--;
-			musicIndex = musicIndex == -1 ? musicData.length - 1 : musicIndex;
-			loadMusic(musicIndex);
-			playMusic();
-		}
-	}
+    function nextAndPrevSong(value) {
+        if (value == 'next') {
+            musicIndex++;
+            musicIndex = musicIndex < musicData.length ? musicIndex : 0;
+            loadMusic(musicIndex);
+            playMusic();
+        } else {
+            musicIndex--;
+            musicIndex = musicIndex == -1 ? musicData.length - 1 : musicIndex;
+            loadMusic(musicIndex);
+            playMusic();
+        }
+    }
 
-	nextBtn.addEventListener('click', () => {
-		nextAndPrevSong('next');
-	});
+    nextBtn.addEventListener('click', () => {
+        nextAndPrevSong('next');
+    });
 
-	prevBtn.addEventListener('click', () => {
-		nextAndPrevSong();
-	});
+    prevBtn.addEventListener('click', () => {
+        nextAndPrevSong();
+    });
 
-	musicListBtn.addEventListener('click', () => {
-		musicListBox.classList.add('active');
-	});
+    musicListBtn.addEventListener('click', () => {
+        musicListBox.classList.add('active');
+    });
 
-	musicBoxClose.addEventListener('click', () => {
-		musicListBox.classList.remove('active');
-	});
+    musicBoxClose.addEventListener('click', () => {
+        musicListBox.classList.remove('active');
+    });
 
-	myAudio.addEventListener('timeupdate', (event) => {
-		const currTime = event.target.currentTime;
-		const duration = event.target.duration;
-		const percentage = (currTime / duration) * 100;
+    myAudio.addEventListener('timeupdate', (event) => {
+        const currTime = event.target.currentTime;
+        const { duration } = event.target;
+        const percentage = (currTime / duration) * 100;
 
-		progressLine.style.width = `${percentage}%`;
+        progressLine.style.width = `${percentage}%`;
 
-		let audioDuration = myAudio.duration;
-		let totalMin = Math.floor(audioDuration / 60);
-		let totalSec = Math.floor(audioDuration % 60);
-		totalTime.innerText = `${totalMin}:${totalSec}`;
+        const audioDuration = myAudio.duration;
+        const totalMin = Math.floor(audioDuration / 60);
+        const totalSec = Math.floor(audioDuration % 60);
+        totalTime.innerText = `${totalMin}:${totalSec}`;
 
-		let currentMin = Math.floor(currTime / 60);
-		let currentSec = Math.floor(currTime % 60);
-		if (currentSec < 10) {
-			currentSec = `0${currentSec}`;
-		}
-		startTime.innerText = `${currentMin}:${currentSec}`;
-	});
+        const currentMin = Math.floor(currTime / 60);
+        let currentSec = Math.floor(currTime % 60);
+        if (currentSec < 10) {
+            currentSec = `0${currentSec}`;
+        }
+        startTime.innerText = `${currentMin}:${currentSec}`;
+    });
 
-	myAudio.addEventListener('canplaythrough', () => {
-		let audioDuration = myAudio.duration;
-		let totalMin = Math.floor(audioDuration / 60);
-		let totalSec = Math.floor(audioDuration % 60);
-		totalTime.innerText = `${totalMin}:${totalSec}`;
-	});
+    myAudio.addEventListener('canplaythrough', () => {
+        const audioDuration = myAudio.duration;
+        const totalMin = Math.floor(audioDuration / 60);
+        const totalSec = Math.floor(audioDuration % 60);
+        totalTime.innerText = `${totalMin}:${totalSec}`;
+    });
 
-	// myAudio.addEventListener('timeupdate', (even) => {
-	// 	const currTime = even.target.currentTime;
-	// 	const duration = even.target.duration;
-	// 	const percentage = (currTime / duration) * 100;
+    // myAudio.addEventListener('timeupdate', (even) => {
+    // 	const currTime = even.target.currentTime;
+    // 	const duration = even.target.duration;
+    // 	const percentage = (currTime / duration) * 100;
 
-	// 	progressLine.style.width = `${percentage}%`;
+    // 	progressLine.style.width = `${percentage}%`;
 
-	// 	myAudio.addEventListener('loadeddata', () => {
-	// 		let audioDuration = myAudio.duration;
-	// 		let totalMin = Math.floor(audioDuration / 60);
-	// 		let totalSec = Math.floor(audioDuration % 60);
-	// 		totalTime.innerText = `${totalMin}:${totalSec}`;
-	// 	});
+    // 	myAudio.addEventListener('loadeddata', () => {
+    // 		let audioDuration = myAudio.duration;
+    // 		let totalMin = Math.floor(audioDuration / 60);
+    // 		let totalSec = Math.floor(audioDuration % 60);
+    // 		totalTime.innerText = `${totalMin}:${totalSec}`;
+    // 	});
 
-	// 	let currentMin = Math.floor(currTime / 60);
-	// 	let currentSec = Math.floor(currTime % 60);
-	// 	if (currentSec < 10) {
-	// 		currentSec = `0${currentSec}`;
-	// 	}
-	// 	startTime.innerText = `${currentMin}:${currentSec}`;
-	// });
+    // 	let currentMin = Math.floor(currTime / 60);
+    // 	let currentSec = Math.floor(currTime % 60);
+    // 	if (currentSec < 10) {
+    // 		currentSec = `0${currentSec}`;
+    // 	}
+    // 	startTime.innerText = `${currentMin}:${currentSec}`;
+    // });
 
-	progressArea.addEventListener('click', (e) => {
-		let progressWidthVal = progressArea.clientWidth;
-		let clickedOffSetX = e.offsetX;
-		let songDuration = myAudio.duration;
-		myAudio.currentTime =
-			(clickedOffSetX / progressWidthVal) * songDuration;
-	});
+    progressArea.addEventListener('click', (e) => {
+        const progressWidthVal = progressArea.clientWidth;
+        const clickedOffSetX = e.offsetX;
+        const songDuration = myAudio.duration;
+        myAudio.currentTime = (clickedOffSetX / progressWidthVal) * songDuration;
+    });
 
-	musicDirectionBtn.addEventListener('click', () => {
-		let getText = musicDirectionBtn.innerText;
+    musicDirectionBtn.addEventListener('click', () => {
+        const getText = musicDirectionBtn.innerText;
 
-		switch (getText) {
-			case 'repeat':
-				musicDirectionBtn.innerText = 'repeat_one';
-				musicDirectionBtn.setAttribute('title', 'song looped');
-				break;
-			case 'repeat_one':
-				musicDirectionBtn.innerText = 'shuffle';
-				musicDirectionBtn.setAttribute('title', 'playback shuffle');
-				break;
-			case 'shuffle':
-				musicDirectionBtn.innerText = 'repeat';
-				musicDirectionBtn.setAttribute('title', 'playlist looped');
-				break;
-		}
-	});
+        switch (getText) {
+            case 'repeat':
+                musicDirectionBtn.innerText = 'repeat_one';
+                musicDirectionBtn.setAttribute('title', 'song looped');
+                break;
+            case 'repeat_one':
+                musicDirectionBtn.innerText = 'shuffle';
+                musicDirectionBtn.setAttribute('title', 'playback shuffle');
+                break;
+            case 'shuffle':
+                musicDirectionBtn.innerText = 'repeat';
+                musicDirectionBtn.setAttribute('title', 'playlist looped');
+                break;
+        }
+    });
 
-	myAudio.addEventListener('ended', () => {
-		let getText = musicDirectionBtn.innerText;
+    myAudio.addEventListener('ended', () => {
+        const getText = musicDirectionBtn.innerText;
 
-		switch (getText) {
-			case 'repeat':
-				nextAndPrevSong('next');
-				break;
-			case 'repeat_one':
-				myAudio.currentTime = 0;
-				loadMusic(musicIndex);
-				playMusic();
-				break;
-			case 'shuffle':
-				let randomIndex = Math.floor(Math.random() * musicData.length);
-				do {
-					randomIndex = Math.floor(Math.random() * musicData.length);
-				} while (musicIndex === randomIndex);
-				musicIndex = randomIndex;
-				console.log(musicIndex);
-				loadMusic(musicIndex);
-				playMusic();
-				break;
-		}
-	});
+        switch (getText) {
+            case 'repeat':
+                nextAndPrevSong('next');
+                break;
+            case 'repeat_one':
+                myAudio.currentTime = 0;
+                loadMusic(musicIndex);
+                playMusic();
+                break;
+            case 'shuffle':
+                let randomIndex = Math.floor(Math.random() * musicData.length);
+                do {
+                    randomIndex = Math.floor(Math.random() * musicData.length);
+                } while (musicIndex === randomIndex);
+                musicIndex = randomIndex;
+                console.log(musicIndex);
+                loadMusic(musicIndex);
+                playMusic();
+                break;
+        }
+    });
+
+    const ulTag = wrapper.querySelector('ul');
+
+    for (let index = 0; index < musicData.length; index++) {
+        const element = `<li>
+							<div class="row">
+								<span>${musicData[index].name}</span>
+								<p>${musicData[index].artist}</p>
+							</div>
+							<audio class="${musicData[index].id}" src="${musicData[index].src}"></audio>
+							<span id="${musicData[index].id}" class="audio-duration">3:40</span>
+						</li>`;
+        ulTag.insertAdjacentHTML('beforeend', element);
+        const liAudioDuration = ulTag.querySelector(`#${musicData[index].id}`);
+        const liAudioTag = ulTag.querySelector(`.${musicData[index].id}`);
+
+        liAudioTag.addEventListener('loadeddata', () => {
+            const audioDuration = liAudioTag.duration;
+            const totalMin = Math.floor(audioDuration / 60);
+            const totalSec = Math.floor(audioDuration % 60);
+            liAudioDuration.innerText = `${totalMin}:${totalSec}`;
+        });
+    }
 }
 
 musicPlayer();
